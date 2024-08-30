6391252c8f17da690f9d75388a135aad86a33c4d
dshem <dshem@gmx.fr>
2024-08-30 19:14:52 +0200

    tmux

--- home/modules/tmux/default.nix
+++ home/modules/tmux/default.nix
@@ -1,8 +1,15 @@
-{
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     keyMode = "vi";
     mouse = true;
     terminal = "tmux-256color";
+    plugins = with pkgs; [
+      tmuxPlugins.yank
+      tmuxPlugins.tmux-fzf
+      tmuxPlugins.fingers
+      tmuxPlugins.catppuccin
+      tmuxPlugins.extrakto
+    ];
   };
 }
