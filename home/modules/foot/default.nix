e75da7e900489a064b670ac4384568ecfbf37d8a
dshem <dshem@gmx.fr>
2024-08-31 14:56:43 +0200

    foot terminal

---
+++ home/modules/foot/default.nix
@@ -0,0 +1,13 @@
+{
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMono Nerd Font:size=14";
+        font-size-adjustment = 1;
+      };
+
+      mouse.hide-when-typing = "yes";
+    };
+  };
+}
