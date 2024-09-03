0961d502bfa42bc89d392626f45f4ed5c26feee9
dshem <dshem@gmx.fr>
2024-09-03 20:55:24 +0200

    back to wezterm

--- home/modules/wezterm/default.nix
+++ home/modules/wezterm/default.nix
@@ -3,7 +3,9 @@
     enable = true;
     extraConfig = ''
       local config = wezterm.config_builder()
-      config.enable_wayland = false
+      config.enable_wayland = true
+      config.hide_tab_bar_if_only_one_tab = true
+      config.window_decorations = "RESIZE"
       config.front_end = "WebGpu"
       config.color_scheme = "Catppuccin Mocha"
       config.window_padding = {
